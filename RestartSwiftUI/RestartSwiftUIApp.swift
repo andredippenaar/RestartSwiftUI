f8cffe806ebc11980186f40f713b7efcf4ec5fd3
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2021-10-23 13:20:30 +0200

    Initial Commit

---
+++ RestartSwiftUI/RestartSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartSwiftUIApp.swift
+//  RestartSwiftUI
+//
+//  Created by Andre Dippenaar on 2021/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
